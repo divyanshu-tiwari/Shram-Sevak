458602269f98e560ee79321cb652ef29ab3ffee8
Divyanshu Tiwari <33171967+divyanshu-tiwari@users.noreply.github.com>
2023-08-14 19:20:40 +0530

    Initial commit to bootstrap client and server app

---
+++ server/service-registry/src/test/java/com/shramsevak/serviceRegistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shramsevak.serviceRegistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
