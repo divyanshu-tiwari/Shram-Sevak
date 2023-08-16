520f0de3211871a12f27f8976073730fa837bef4
ashish-thattikota3101 <132429752+ashish-thattikota3101@users.noreply.github.com>
2023-08-16 13:41:24 +0530

    bootstraped worker service

---
+++ server/worker-service/src/main/java/com/shramsevak/workerService/service/WorkerService.java
@@ -0,0 +1,5 @@
+package com.shramsevak.workerService.service;
+
+public interface WorkerService {
+
+}
---
+++ server/worker-service/src/main/java/com/shramsevak/workerService/service/WorkerServiceImpl.java
@@ -0,0 +1,5 @@
+package com.shramsevak.workerService.service;
+
+public class WorkerServiceImpl implements WorkerService {
+
+}
