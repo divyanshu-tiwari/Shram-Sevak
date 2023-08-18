a94f19893fcb990ace4bd6788ff5d21fb842ed7c
TarjanSahu <tarjansahu6@gmail.com>
2023-08-18 13:36:44 +0530

    Some minor correction

--- server/customer-service/src/main/java/com/shramsevek/customerEntity/Customer.java
+++ server/customer-service/src/main/java/com/shramsevek/customerEntity/Customer.java
@@ -5,13 +5,13 @@ import lombok.*;
 
 @NoArgsConstructor
 @AllArgsConstructor
-@Getter
-@Setter
+@Data
 @Entity
 @Table(name ="customers")
 public class Customer {
 
-@Column(length = 36)
+//Number validation required either FrontEnd Or BackEnd
+@Id
 private int customerId;
 
 @Column(length = 36 ,nullable=false)
@@ -26,14 +26,13 @@ private String email;
 @Column(length = 36 ,nullable=false)
 private String password;
 
-@Column(length = 128 ,nullable=false)
+@Column(nullable=false)
 private int contact;
 
 @Enumerated(EnumType.STRING)
-@Column(length = 16 ,nullable=false)
+@Column(nullable=false)
 private Gender gender;
 
-@Column(length = 8)
 private boolean isActive;  
 
 }
--- server/customer-service/src/main/java/com/shramsevek/customerEntity/Gender.java
+++ server/customer-service/src/main/java/com/shramsevek/customerEntity/Gender.java
@@ -1,5 +1,5 @@
 package com.shramsevek.customerEntity;
 
 public enum Gender {
-
+	MALE,FEMALE,OTHERS
 }
