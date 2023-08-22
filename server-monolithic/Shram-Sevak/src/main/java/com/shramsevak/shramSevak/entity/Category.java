f19267961b28e5c735731b356f82a6f0ecdc6ef1
PSGsahil <132652957+PSGsahil@users.noreply.github.com>
2023-08-22 15:29:27 +0530

    getter setter added in category entity

--- server-monolithic/Shram-Sevak/src/main/java/com/shramsevak/shramSevak/entity/Category.java
+++ server-monolithic/Shram-Sevak/src/main/java/com/shramsevak/shramSevak/entity/Category.java
@@ -3,6 +3,8 @@ package com.shramsevak.shramSevak.entity;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.hibernate.annotations.SecondaryRow;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -12,20 +14,22 @@ import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 
 @Entity
 @Table(name = "categories")
+@Getter
+@Setter
+
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-public class Category {
+public class Category extends BaseEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long categoryId;
-
+    
     @Column(nullable = false)
     private String categoryName;
 
