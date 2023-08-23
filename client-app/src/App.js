8167f031c205085a472a3a87de946b0c64e4c636
TarjanSahu <tarjansahu6@gmail.com>
2023-08-23 23:02:22 +0530

    Adding Routing Components for SignUp SignIn for worker and Customer

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import HomePage from './customer/components/Pages/HomePage/HomePage';
 import Navigation from './customer/components/navigation/Navigation';
+import FormWorker from './Worker/components/Login&Registration/FormWorker'
+import FormCustomer from './customer/components/Login&Registration/FormCustomer'
 import { Routes,Route} from "react-router-dom";
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
       <Navigation />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/loginCustomer" element={<FormCustomer />} />
+        <Route path="/loginWorker" element={<FormWorker />} />
       </Routes>
     </>
   );
---
+++ client-app/src/RoutingSignIn-SignUp/Routing.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom'
+import './Style.css';
+import React from 'react'
+import './StyleRouting.css';
+
+const Card = ({ type, imageSrc, onClick }) => {
+  return (
+    <div className={`card ${type}`} onClick={onClick}>
+      <img src={imageSrc} alt={type} className="card-image" />
+      {type}
+    </div>
+  );
+};
+
+
+function Routing() {
+    const navigate = useNavigate();
+
+    const handleClickCustomer = () => {
+      navigate('/FormCustomer')
+    };
+    const handleClickWorker = () => {
+      navigate('/FormWorker')
+    };
+ 
+  return (
+    <div className="card-container">
+    <Card
+      type="customer"
+      imageSrc="./customer-retention-vector-icon-client.avif"
+      onClick={() => {handleClickCustomer}}
+    />
+    <Card
+      type="worker"
+      imageSrc="./download.png"
+      onClick={() => handleClickWorker()}
+    />
+  </div>
+  );
+};
+
+export default Routing
