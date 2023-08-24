4d81fc5c3556cac8d9c1378b954404cf40a09945
Divyanshu Tiwari <33171967+divyanshu-tiwari@users.noreply.github.com>
2023-08-24 22:56:25 +0530

    add auth guard for admin

--- client-app/src/guards/auth.guard.js
+++ client-app/src/guards/auth.guard.js
@@ -2,14 +2,14 @@ import { useSelector } from "react-redux/es/hooks/useSelector";
 import { Navigate } from "react-router";
 
 export const AuthGuard = ({ children, roles }) => {
-    const currentUser = useSelector(selectUser)
+    const currentUser = useSelector((state) => state.user)
 
     const authorize = () => {
-        if(!currentUser){
+        if(!currentUser.value){
             return (<Navigate to={{pathname: '/admin'}} />)
         }
-
-        if(roles?.indexOf(currentUser.role === -1)){
+        alert("In AUTH-GUARD Current user role: " + currentUser.value.role)
+        if(roles?.indexOf(currentUser.value.role) === -1){
             return (<Navigate to={{pathname: '/401'}} />)
         }
 
