e6d71814132d0830e3cf3a01020c8bf6461c8af8
Divyanshu Tiwari <33171967+divyanshu-tiwari@users.noreply.github.com>
2023-08-24 16:53:58 +0530

    add AuthGuard to admin dashboard route

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -3,13 +3,15 @@ import HomePage from './customer/components/Pages/HomePage/HomePage';
 import Navigation from './customer/components/navigation/Navigation';
 import FormWorker from './Worker/components/Login&Registration/FormWorker'
 import FormCustomer from './customer/components/Login&Registration/FormCustomer'
-import { Routes,Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import AdminContainer from "./admin/components/AdminContainer"
 
 import ChooseLogin from './customer/components/Pages/HomePage/ChooseLogin/ChooseLogin';
 import AdminDashboard from './admin/components/AdminDashboard';
-
+import { Unauthorized } from './pages/unauthorized.page';
+import { AuthGuard } from './guards/auth.guard';
+import { Role } from './models/role';
 
 function App() {
   return (
@@ -21,10 +23,16 @@ function App() {
         <Route path="/loginWorker" element={<FormWorker />} />
 
         <Route path="/admin" element={<AdminContainer />} />
-        <Route path='/admin-dashboard' element={<AdminDashboard/>} />
+
+        <Route path='/admin-dashboard' element={
+          <AuthGuard roles={[Role.ADMIN]}>
+            <AdminDashboard />
+          </AuthGuard>
+        } />
 
         <Route path="/login" element={<ChooseLogin />} />
 
+        <Route path="/401" element={<Unauthorized />} />
       </Routes>
     </>
   );
