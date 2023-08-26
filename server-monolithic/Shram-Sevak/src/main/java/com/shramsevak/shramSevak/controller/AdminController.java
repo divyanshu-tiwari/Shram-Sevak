696872be52c64eeabb288bc070395af3beb1a18a
Divyanshu Tiwari <33171967+divyanshu-tiwari@users.noreply.github.com>
2023-08-27 00:32:29 +0530

    add cross origin to admin controller

--- server-monolithic/Shram-Sevak/src/main/java/com/shramsevak/shramSevak/controller/AdminController.java
+++ server-monolithic/Shram-Sevak/src/main/java/com/shramsevak/shramSevak/controller/AdminController.java
@@ -3,6 +3,7 @@ package com.shramsevak.shramSevak.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/admin")
 @Slf4j
 public class AdminController {
