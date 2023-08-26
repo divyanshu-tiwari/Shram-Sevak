b3e8d6dc60c11919390a7eae0522e8b6a86bc0a2
Divyanshu Tiwari <33171967+divyanshu-tiwari@users.noreply.github.com>
2023-08-27 03:37:35 +0530

    Update AdminDashboard.jsx
    remove line CustomerTable reference to resolve error on development

--- client-app/src/admin/pages/AdminDashboard.jsx
+++ client-app/src/admin/pages/AdminDashboard.jsx
@@ -183,7 +183,7 @@ export default function AdminDashboard() {
         </header>
         <main>
           <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-                {currentPage == '/customers' && <CustomerTable /> }
+               
           </div>
         </main>
       </div>
