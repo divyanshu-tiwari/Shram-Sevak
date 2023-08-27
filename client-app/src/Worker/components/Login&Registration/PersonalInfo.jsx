7a0f5a644980b7f9d26e2c949acb7c790a26e11c
Jayesh-Sinnarkar <jayesh.sinnarkar@gmail.com>
2023-08-28 00:40:48 +0530

    Interated admin dashboard with sign out functionality

--- client-app/src/Worker/components/Login&Registration/PersonalInfo.jsx
+++ client-app/src/Worker/components/Login&Registration/PersonalInfo.jsx
@@ -43,7 +43,8 @@ function PersonalInfo({ formData, setFormData }) {
               setFormData({ ...formData, gender: e.target.value });
             }}
           >
-            <option value="MALE" selected>Male</option>
+            
+            <option value="MALE" >Male</option>
             <option value="FEMALE">Female</option>
             <option value="OTHER">Other</option>
           </select>
