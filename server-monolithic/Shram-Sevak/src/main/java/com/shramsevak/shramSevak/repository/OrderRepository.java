c1c88be464ac9679ea782adb66ac4c48396d4311
Divyanshu Tiwari <33171967+divyanshu-tiwari@users.noreply.github.com>
2023-08-28 02:20:55 +0530

    savepoint

--- server-monolithic/Shram-Sevak/src/main/java/com/shramsevak/shramSevak/repository/OrderRepository.java
+++ server-monolithic/Shram-Sevak/src/main/java/com/shramsevak/shramSevak/repository/OrderRepository.java
@@ -6,10 +6,11 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.shramsevak.shramSevak.entity.Order;
+import com.shramsevak.shramSevak.entity.OrderStatus;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
 	public List<Order> findByWorkerId(Long workerId);
 	public List<Order> findByCustomerId(Long customerId);
-	public List<Order> findAllByStartTimeIs(LocalDateTime startTime);
+	public List<Order> findAllByStartTimeIsAndStatus(LocalDateTime startTime, OrderStatus status);
 }
--- server-monolithic/Shram-Sevak/src/main/java/com/shramsevak/shramSevak/service/WorkerServiceImpl.java
+++ server-monolithic/Shram-Sevak/src/main/java/com/shramsevak/shramSevak/service/WorkerServiceImpl.java
@@ -2,7 +2,10 @@ package com.shramsevak.shramSevak.service;
 
 import static com.shramsevak.shramSevak.util.Utils.checkStatus;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import org.modelmapper.ModelMapper;
@@ -16,11 +19,14 @@ import com.shramsevak.shramSevak.customException.WorkerException;
 import com.shramsevak.shramSevak.dto.SigninRequest;
 import com.shramsevak.shramSevak.dto.SigninResponse;
 import com.shramsevak.shramSevak.dto.WorkerRegistrationDto;
-import com.shramsevak.shramSevak.dto.WorkerResponceDto;
+import com.shramsevak.shramSevak.dto.WorkerResponseDTO;
 import com.shramsevak.shramSevak.entity.Locality;
+import com.shramsevak.shramSevak.entity.OrderStatus;
 import com.shramsevak.shramSevak.entity.Worker;
 import com.shramsevak.shramSevak.entity.WorkerStatus;
 import com.shramsevak.shramSevak.repository.LocalityRepository;
+import com.shramsevak.shramSevak.repository.OrderRepository;
+import com.shramsevak.shramSevak.repository.SkillRepository;
 import com.shramsevak.shramSevak.repository.WorkerRepository;
 
 import jakarta.transaction.Transactional;
@@ -31,21 +37,24 @@ public class WorkerServiceImpl implements WorkerService {
 
 	@Autowired
 	private WorkerRepository workerRepo;
-	
 	@Autowired
 	private LocalityRepository localityRepo;
+	@Autowired
+	private SkillRepository skillRepo;
+	@Autowired
+	private OrderRepository orderRepo;
 	
 	@Autowired
 	private ModelMapper mapper;
 	
 	@Override
-	public WorkerResponceDto register(WorkerRegistrationDto workerDto) {
+	public WorkerResponseDTO register(WorkerRegistrationDto workerDto) {
 		Worker worker = mapper.map(workerDto, Worker.class);
 		Locality locality = localityRepo.findById(workerDto.getLocalityId()).orElseThrow(() -> new RuntimeException("Invalid locality ID"));
 		locality.addWorker(worker);
 		worker.setStatus(WorkerStatus.PENDING);
 		workerRepo.save(worker);
-		WorkerResponceDto workerResp=mapper.map(worker, WorkerResponceDto.class);
+		WorkerResponseDTO workerResp=mapper.map(worker, WorkerResponseDTO.class);
 		return workerResp;
 	}
 	
@@ -78,25 +87,36 @@ public class WorkerServiceImpl implements WorkerService {
 	}
 
   @Override
-	public WorkerResponceDto getWorkerDetails(Long id) {
+	public WorkerResponseDTO getWorkerDetails(Long id) {
     Worker worker=workerRepo.findById(id).orElseThrow(() -> new ResourceNotFoundException("Invalid Customer ID!!!"));
 		
-		return mapper.map(worker,WorkerResponceDto.class);
+		return mapper.map(worker,WorkerResponseDTO.class);
 	}
 
 
 	@Override
-	public List<WorkerResponceDto> getAllWorkers(int pageNumber, int pageSize) {
+	public List<WorkerResponseDTO> getAllWorkers(int pageNumber, int pageSize) {
 		
      Pageable pageable = PageRequest.of(pageNumber, pageSize);
 		
 		List<Worker> workerList = workerRepo.findAll(pageable).getContent();
 		return workerList.stream().
-				map( worker -> mapper.map(worker, WorkerResponceDto.class))
+				map( worker -> mapper.map(worker, WorkerResponseDTO.class))
 				.collect(Collectors.toList());
 		
 	}
+
+
+	@Override
+	public List<WorkerResponseDTO> getAvailableWorkersBySlotAndSkill(Long skillId, LocalDateTime startTime, LocalDateTime endTime, int pageNumber, int pageSize) {
+		Set<Worker> workersWithDesiredSkill = skillRepo.findById(skillId).orElseThrow(() -> new ResourceNotFoundException("No such skill found.")).getWorkers(); 
+		Set<Worker> unavailableWorkers = orderRepo.findAllByStartTimeIsAndStatus(startTime, OrderStatus.CONFIRMED).stream().map(order -> order.getWorker()).collect(Collectors.toSet());
+		List<Worker> availableWorkers = workersWithDesiredSkill.stream().filter(Predicate.not(unavailableWorkers::contains)).distinct().toList();
+		return availableWorkers.stream().map(worker -> mapper.map(worker, WorkerResponseDTO.class)).collect(Collectors.toList());
+	}
+
+
+
 	
-		
 
 }
