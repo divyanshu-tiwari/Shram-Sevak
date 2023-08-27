ee31c766ceb122d50c85afe77ef85809bcdb89be
PSGsahil <132652957+PSGsahil@users.noreply.github.com>
2023-08-28 02:50:05 +0530

    payment gateway added

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -17,7 +17,7 @@ import { AdminAuthGuard } from './utils/guards/admin.auth.guard';
 import { AdminLoginGuard } from './utils/guards/admin.login.guard';
 import { Role } from './utils/models/role';
 import RegistrationSuccess from './Worker/components/Login&Registration/RegistrationSuccess';
-
+import Cart from './customer/components/Cart/Cart';
 
 function App() {
   return (
@@ -44,6 +44,7 @@ function App() {
       <Route path="/login" element={<ChooseLogin />} />
 
       <Route path="/401" element={<Unauthorized />} />
+      <Route path='/payment' element={<Cart></Cart>}/>
     </Routes >
 
     </>
