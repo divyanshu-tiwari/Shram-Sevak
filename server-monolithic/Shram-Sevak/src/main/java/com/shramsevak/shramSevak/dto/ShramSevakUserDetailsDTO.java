8c4c6e037d7413b109fed75cee29acee06386dff
Divyanshu Tiwari <33171967+divyanshu-tiwari@users.noreply.github.com>
2023-09-02 00:10:11 +0530

    add ShramSevakUserDetailsDTO
    - DTO is provided to securityContextHolder as principal

---
+++ server-monolithic/Shram-Sevak/src/main/java/com/shramsevak/shramSevak/dto/ShramSevakUserDetailsDTO.java
@@ -0,0 +1,11 @@
+package com.shramsevak.shramSevak.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class ShramSevakUserDetailsDTO {
+	private String username;
+	private Long userId;
+}
