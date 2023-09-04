ce499612a0f84cbfa3268140fbe33b1099ba117d
Divyanshu Tiwari <33171967+divyanshu-tiwari@users.noreply.github.com>
2023-09-05 00:47:57 +0530

    add security config with valid config for admin

--- server-monolithic/Shram-Sevak/src/main/java/com/shramsevak/shramSevak/security/SecurityConfig.java
+++ server-monolithic/Shram-Sevak/src/main/java/com/shramsevak/shramSevak/security/SecurityConfig.java
@@ -1,5 +1,92 @@
 package com.shramsevak.shramSevak.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
+	
+	@Configuration
+	@Order(1)
+	public static class AdminSecurityConfig {
+	
+		@Autowired
+		private JWTRequestFilter jwtRequestFilter;
+		
+		@Bean
+		public SecurityFilterChain authorizeRequest(HttpSecurity http) throws Exception {
+			http
+			.csrf(csrf -> csrf.disable())
+			.authorizeHttpRequests(authz -> authz
+					.requestMatchers("/admin").hasRole("ADMIN")
+//					.requestMatchers("")
+					.anyRequest().authenticated()
+					
+					)
+			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+			
+			return http.build();
+		}
+	}
+	
+	
+	@Configuration
+	@Order(2)
+	public static class CustomerSecurityConfig {
+	
+		@Autowired
+		private JWTRequestFilter jwtRequestFilter;
+		
+		@Bean
+		public SecurityFilterChain authorizeRequest(HttpSecurity http) throws Exception {
+			http
+			.csrf(csrf -> csrf.disable())
+			.authorizeHttpRequests(authz -> authz
+					.requestMatchers("/admin").hasRole("ADMIN")
+//					.requestMatchers("")
+					.anyRequest().authenticated()
+					
+					)
+			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+			
+			return http.build();
+		}
+	}
+	
+	
+	@Configuration
+	@Order(3)
+	public static class WorkerSecurityConfig {
+	
+		@Autowired
+		private JWTRequestFilter jwtRequestFilter;
+		
+		@Bean
+		public SecurityFilterChain authorizeRequest(HttpSecurity http) throws Exception {
+			http
+			.csrf(csrf -> csrf.disable())
+			.authorizeHttpRequests(authz -> authz
+					.requestMatchers("/admin").hasRole("ADMIN")
+//					.requestMatchers("")
+					.anyRequest().authenticated()
+					
+					)
+			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+			
+			return http.build();
+		}
+	}
+	
 }
